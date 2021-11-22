def23e3e77917b7a224dd47e64a4637c0804400f
WSH95 <d201780203@hust.edu.cn>
2021-11-22 22:10:53 +0800

    initial commit

---
+++ test_example/gen_IK.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+
+. /home/wsh/Documents/myRosProj/test_ws/install/setup.bash
+
+function func1()
+{
+#   if [ "${1##*.}" = "urdf" ]; then
+  if [ -f $1 ]; then
+    echo "found $1"
+    local name=${1%.*}
+    if [ -d $name ]; then
+      rm -rf $name
+    fi
+    mkdir -p $name && cd $name
+    ros2 run collada_urdf urdf_to_collada ../$name.urdf $name.dae
+    python `openrave-config --python-dir`/openravepy/_openravepy_/ikfast.py --robot=$name.dae --iktype=translation3d --baselink=0 --eelink=4 --savefile=ikfast_$name.cpp
+    cp /usr/local/lib/python2.7/dist-packages/openravepy/_openravepy_/ikfast.h .
+    g++ ikfast_$name.cpp -o ikfast_$name -llapack -std=c++11
+    cd ..
+  else
+    echo "There is no $1 in current dir"
+  fi
+}
+
+if [ $# -eq 0 ];then
+  for file in $(ls *.urdf)
+  do
+    func1 ${file}
+  done
+else
+  for i in "$@"
+  do
+    func1 "${i%.*}.urdf"
+  done
+fi
